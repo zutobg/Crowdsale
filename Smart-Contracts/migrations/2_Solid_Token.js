424fcc62881a009e3d0362135125177da6f3d101
JG Carvalho <jgcarvalho93@gmail.com>
2018-09-06 18:19:56 -0300

    fixed compiling issues

--- Smart-Contracts/migrations/2_Solid_Token.js
+++
@@ -1,9 +0,0 @@
-const SolidToken = artifacts.require('./SolidToken.sol');
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(SolidToken)
-  .then((instance) => {
-    console.log("Deploying Solid token");
-    console.log("Solid token address: ", instance.address);
-  })
-}
